bf3038ad78615895abb83ccf3f08c2ae5b8dc367
Mahdi Yusuf <yusuf.mahdi@gmail.com>
2012-01-06 15:19:21 -0500

    Update courtside/keys/keys.py

--- courtside/keys/keys.py
+++ courtside/keys/keys.py
@@ -11,15 +11,3 @@ if settings.DEBUG is False:
     #twitter information
     CONSUMER_KEY = 'WSKcN7DBwH88GkZc2wT24g'
     CONSUMER_SECRET = 'VF1eDbszUthnUrMy3693dU2iSWCK4qQBfvmreCXdhGU'
-
-else:
-    #facebook information 
-    FACEBOOK_APP_ID = '202475996473036'
-    FACEBOOK_API_KEY = '202475996473036'
-    FACEBOOK_APP_SECRET = 'd379eb66ef57dca6714b81c8a4afd23f'
-
-    #twitter information
-    CONSUMER_KEY = '8S4oIhhl7ap5lboUk6N8w'
-    CONSUMER_SECRET = '8rEgNAbwXXecrPweEPyaT5Oproxwj31ZAAb0qvgAuY'
-
-SUPPORT_PASSWORD = 'aew7Seey'
