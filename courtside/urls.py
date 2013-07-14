787aa8ed25016f6e04ba3aea5f73b25fd056e5ff
Mahdi Yusuf <yusuf.mahdi@gmail.com>
2013-07-14 15:46:31 -0400

    removing use of celery and tastypie

--- courtside/urls.py
+++ courtside/urls.py
@@ -1,45 +1,33 @@
 from django.conf.urls.defaults import patterns, include, url
-from django.conf import settings
 from django.contrib import admin
-from tastypie.api import Api
-
-from game.api import (GameResource, UserResource, PlayerResource,
-SportResource)
-
-v1_api = Api(api_name='v1')
-v1_api.register(GameResource())
-v1_api.register(UserResource())
-v1_api.register(PlayerResource())
-v1_api.register(SportResource())
-
-
 admin.autodiscover()
 
 urlpatterns = patterns('',
-    # Examples:
-    # url(r'^$', 'courtside.views.home', name='home'),
-    url(r'^about/', 'game.views.about', name='about'),
 
+    url(r'^about/', 'game.views.about', name='about'),
     url(r'^$', 'game.views.home', name='home'),
-    
+
     url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
     url(r'^admin/', include(admin.site.urls)),
 
     #regular signup
-    url(r'^login/signup/', 'register.views.new_register', name='regular_signup'),
+    url(r'^login/signup/', 'register.views.new_register',
+        name='regular_signup'),
 
     #login complete
     url(r'^login/complete/', 'register.views.complete_sign_up'),
 
     #twitter stuff
-	url(r'^login/twitter/', 'register.views.begin_twitter_oauth', name='twitter_oauth'),
+    url(r'^login/twitter/', 'register.views.begin_twitter_oauth',
+        name='twitter_oauth'),
     url(r'^twitter/callback/', 'register.views.twitter_callback'),
-    
+
     #edit profile
     url(r'^profile/edit/', 'register.views.profile_edit'),
 
     #facebook stuff
-    url(r'^login/facebook/', 'register.views.begin_facebook_oauth', name='facebook_oauth'),
+    url(r'^login/facebook/', 'register.views.begin_facebook_oauth',
+        name='facebook_oauth'),
     url(r'^facebook/callback/', 'register.views.facebook_callback'),
 
     url(r'^logout/', 'register.views.logout_bro', name='logout'),
@@ -53,12 +41,7 @@ urlpatterns = patterns('',
     url(r'^leave/game/(?P<id>\d+)/$', 'game.views.leave', name='leave'),
     url(r'^delete/game/(?P<id>\d+)/$', 'game.views.delete', name='delete'),
 
-    # comments application 
+    # comments application
     (r'^comments/', include('django.contrib.comments.urls')),
-
-    #api endpoints
-     (r'^api/', include(v1_api.urls)),
-
 )
-
 
