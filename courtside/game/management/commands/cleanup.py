9c4f44342fb3263b08b62dd26600aa07f25751f5
Mahdi Yusuf <yusuf.mahdi@gmail.com>
2013-12-03 23:02:39 -0500

    adding code for celery 2

---
+++ courtside/game/management/commands/cleanup.py
@@ -0,0 +1,18 @@
+from django.core.management.base import BaseCommand
+from register.tasks import CleanGamesTask
+from game.models import Game
+
+
+class Command(BaseCommand):
+    """
+    This is a management command that marks games as inactive
+    after they have happened.
+    """
+    args = ''
+    help = 'Cleans up finished game from the system'
+
+    def handle(self, *args, **kwargs):
+        games = Game.objects.filter(active=True).values('id')
+        games = [game['id'] for game in games]
+        print games
+        CleanGamesTask().delay(games)
--- courtside/register/tasks.py
+++ courtside/register/tasks.py
@@ -1,3 +1,5 @@
+from time import sleep
+from delorean import Delorean
 from celery.registry import tasks
 from celery.task import Task
 
@@ -5,6 +7,8 @@ from django.template.loader import render_to_string
 from django.utils.html import strip_tags
 
 from django.core.mail import EmailMultiAlternatives
+
+from game.models import Game
 
 class SignUpTask(Task):
 
@@ -20,6 +24,25 @@ class SignUpTask(Task):
 
 tasks.register(SignUpTask)
 
+
+class CleanGamesTask(Task):
+    def run(self, game_ids):
+        # sleep for 15 seconds
+        sleep(15)
+        for game_id in game_ids:
+            try:
+                game = Game.objects.get(id=game_id)
+            except:
+                continue
+            now = Delorean().naive()
+            if game.start_date_and_time > now:
+                print 'game %s has not occured yet!' % game.id
+            else:
+                game.active = False
+            game.save()
+
+
+tasks.register(CleanGamesTask)
 
 
 
