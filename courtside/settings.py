6d427391c543cc602ae2d92e1aa61ea15721645b
Mahdi Yusuf <yusuf.mahdi@gmail.com>
2015-02-03 16:22:20 -0500

    Update settings.py

--- courtside/settings.py
+++ courtside/settings.py
@@ -28,18 +28,6 @@ if DEBUG:
         }
     }
 
-else:
-    DATABASES = {
-        'default': {
-            'ENGINE': 'django.db.backends.postgresql_psycopg2',
-            'NAME': 'db0pkdtmic9k69',                       # Or path to database file if using sqlite3.
-            'USER': 'cgurmijgbhnmwx',                       # Not used with sqlite3.
-            'PASSWORD': '4ROnFFHYIkPWWeyhtE8eFpJGxT',                   # Not used with sqlite3.
-            'HOST': 'ec2-54-204-16-70.compute-1.amazonaws.com',                       # Set to empty string for localhost. Not used with sqlite3.
-            'PORT': '5432',                       # Set to empty string for default. Not used with sqlite3.
-        }
-    }
-
 # Hosts/domain names that are valid for this site; required if DEBUG is False
 # See https://docs.djangoproject.com/en/1.4/ref/settings/#allowed-hosts
 ALLOWED_HOSTS = ['*']
